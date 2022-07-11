03b5a382cb826fd4cac7c8f216a01802787edd8b
Guillem Cordoba <guillem.cordoba@gmail.com>
2022-07-11 05:32:53 +0200

    Exercises and test

--- src/lib.rs
+++ src/lib.rs
@@ -65,7 +65,6 @@ pub fn is_prime(n: u32) -> bool {
 // Eg: nth_prime(1) = 2
 // Eg: nth_prime(2) = 3
 // Eg: nth_prime(3) = 5
-// QUESTION: Maybe extra curriculum?
 pub fn nth_prime(n: u32) -> u32 {
     let mut primes_found = 0;
     let mut i = 1;
@@ -80,58 +79,6 @@ pub fn nth_prime(n: u32) -> u32 {
 
     i
 }
-
-// QUESTION: Closure exercise? Seems too advanced here?
-
-/**
- * MOVE SEMANTICS
- * ==============
- *
- *
- */
-
-/// Ownership
-
-//
-pub fn double(n: u32) -> u32 {
-    n + n
-}
-
-// Appends the given char "c" to the given String "s"
-// Eg: append_char(String::from("hello"), '0') = String::from("hello0")
-pub fn append_char(s: String, c: char) -> String {
-    let mut s = s;
-
-    s.push(c);
-
-    s
-}
-
-fn merge_strings(mut s1: String, s2: String) -> String {
-    s1.push_str(s2.as_str());
-
-    s2
-}
-
-// Eg: double_append_char(String::from("hello"), '1', '0') = String::from("hello0hello1")
-pub fn double_append_char(s: String, c1: char, c2: char) -> String {
-    let s1 = append_char(s.clone(), c1);
-    let s2 = append_char(s, c2);
-
-    merge_strings(s1, s2)
-}
-
-// TODO: how to make exercise for which you have to exercise ownership
-
-/// References
-
-// Appends the given char "c" to the given String "s"
-// Eg: let s = String::from("hello"); append_char(, '0') = String::from("hello0")
-pub fn append_char_mut(s: &mut String, c: char) -> () {
-    s.push(c);
-}
-
-// TODO: add more exercises for references
 
 /**
  * PRIMITIVE COMPOUND TYPES
@@ -148,7 +95,7 @@ pub fn return_first_member(tuple: (u32, u32)) -> u32 {
 }
 
 // Return the sum of all the members of the tuple
-// Eg: sum((1, 2, 3)) = 6
+// Eg: sum((1, 2, 3)) == 6
 pub fn sum_members(tuple: (u32, u32, u32)) -> u32 {
     let (first, second, third) = tuple;
 
@@ -156,13 +103,13 @@ pub fn sum_members(tuple: (u32, u32, u32)) -> u32 {
 }
 
 // Build a tuple of 3 members that are all equal to the given number
-// Eg: triplicate_number(1) = (1, 1, 1)
+// Eg: triplicate_number(1) == (1, 1, 1)
 pub fn triplicate_number(n: u32) -> (u32, u32, u32) {
     (n, n, n)
 }
 
 // Build a tuple of 3 members that are all equal to the given string
-// Eg: triplicate_string(String::from("a")) = (String::from("a"), String::from("a"), String::from("a"))
+// Eg: triplicate_string(String::from("a")) == (String::from("a"), String::from("a"), String::from("a"))
 pub fn triplicate_string(s: String) -> (String, String, String) {
     (s.clone(), s.clone(), s)
 }
@@ -170,19 +117,19 @@ pub fn triplicate_string(s: String) -> (String, String, String) {
 /// Arrays
 
 // Return an array of 10 elements, and fill it with the given number
-// Eg: build_array(2) = [2, 2, 2, 2, 2]
+// Eg: build_array(2) == [2, 2, 2, 2, 2]
 pub fn build_array(n: u32) -> [u32; 5] {
     [n; 5]
 }
 
 // Return the nth element of the array
-// Eg: nth_element([2, 5, 3, 8, 1], 2) = 3
+// Eg: nth_element([2, 5, 3, 8, 1], 2) == 3
 pub fn nth_element(array: [u32; 5], n: usize) -> u32 {
     array[n]
 }
 
 // Return the maximum number in the given array
-// Eg: max_in_array([2, 5, 3, 8, 1]) = 8
+// Eg: max_in_array([2, 5, 3, 8, 1]) == 8
 pub fn max_in_array(array: [u32; 5]) -> u32 {
     let mut max = 0;
 
@@ -195,159 +142,37 @@ pub fn max_in_array(array: [u32; 5]) -> u32 {
     max
 }
 
-/**
- * NON PRIMITIVE NATIVE TYPES
- * ==========================
- */
-
-/// String literal
-
-// Convert the given string literal to a string struct
-pub fn convert_to_string_struct(string_literal: &str) -> String {
-    String::from(string_literal)
-}
-
-// Append the word "world" to the given string
-// Eg: append_world(String::from("hello, ")) = String::from("hello, world")
-pub fn append_world(mut s: String) -> String {
-    s.push_str("world");
-
-    s
-}
-
 // Returns true if the given string literal is either "foo" or "bar"
-// Eg: is_foo_or_bar("foo") = true
-// Eg: is_foo_or_bar("bar") = true
-// Eg: is_foo_or_bar("another string") = false
+// Eg: is_foo_or_bar("foo") == true
+// Eg: is_foo_or_bar("bar") == true
+// Eg: is_foo_or_bar("another string") == false
 pub fn is_foo_or_bar(s: &str) -> bool {
     s == "foo" || s == "bar"
 }
 
-/// Structs
-
-pub struct Person {
-    pub name: String,
-    pub age: u32,
-}
-
-//
-// Eg: longest_name(Person::new(String::from("Marcus"), 23), Person::new(String::from("William"), 33)) = String::from("William")
-pub fn longest_name(person1: &Person, person2: &Person) -> String {
-    if person1.name.len() < person2.name.len() {
-        person2.name.clone()
-    } else {
-        person1.name.clone()
-    }
-}
-
-impl Person {
-    // Returns whether this person is less than 18 years old
-    // Eg: is_underage(Person::new(String::from("Niki"), 19)) = false
-    pub fn is_underage(&self) -> bool {
-        self.age < 18
-    }
-
-    // Sets the age for this person
-    pub fn set_age(&mut self, age: u32) -> () {
-        self.age = age;
-    }
-}
-
-// TODO: tuple and unit type variants
-
-/// Strings
-
-// Returns a greeting for the given person
-// Eg: greet(Person::new(String::from("Tony"), 43)) = String::from("Hello Tony, you are 43 years old!")
-pub fn greet(person: Person) -> String {
-    format!("Hello {}, you are {} years old!", person.name, person.age)
-}
-
-// TODO: Add more exercises
-
-pub struct Rectangle {
-    pub width: f32,
-    pub height: f32,
-}
-
-impl Rectangle {
-    // Return a new instance of the Rectangle struct
-    pub fn new(width: f32, height: f32) -> Rectangle {
-        Rectangle { width, height }
-    }
-
-    // Compute the area for this shape
-    pub fn area(&self) -> f32 {
-        self.width * self.height
-    }
-}
-
-/// Enums
-
-pub enum Operation {
-    Increment,
-    Decrement,
-}
-
-// Count the number of Increment operations for the given array
-// Eg: count_increments([Operation::Increment, Operation::Decrement, Operation::Increment]) = 2
-pub fn count_increments(operations: [Operation; 3]) -> u32 {
-    let mut increments = 0;
-
-    for op in operations {
-        if let Operation::Increment = op {
-            increments += 1;
+// Returns true if the given string literal contains the given character
+// Eg: contains_char("foo", 'f') == true
+// Eg: contains_char("foo", 's') == false
+pub fn contains_char(s: &str, character: char) -> bool {
+    for c in s.chars() {
+        if c == character {
+            return true;
         }
     }
-
-    increments
+    false
 }
 
-// Perform the given operation on the given counter
-// Eg: perform_operation(4, Operation::Increment) = 5
-// Eg: perform_operation(0, Operation::Decrement) = 0 (!!)
-pub fn perform_operation(counter: u32, operation: Operation) -> u32 {
-    match operation {
-        Operation::Increment => counter + 1,
-        Operation::Decrement if counter == 0 => 0,
-        Operation::Decrement => counter - 1,
-    }
-}
+// Returns true if the given string literal consists of just its first half repeteated twice
+// Eg: is_first_half_repeated("fofo") == true
+// Eg: is_first_half_repeated("ff") == true
+// Eg: is_first_half_repeated("fof") == false
+// Eg: is_first_half_repeated("fofoo") == false
+// Eg: is_first_half_repeated("foo") == false
+pub fn is_first_half_repeated(s: &str) -> bool {
+    let half = s.len() / 2;
 
-///
-
-pub enum Shape {
-    // TODO: remove variants
-    Rectangle { width: f32, height: f32 },
-    Square { size: f32 },
-    Triangle { base: f32, height: f32 },
-}
-
-impl Shape {
-    // Compute the area for this shape
-    pub fn area(&self) -> f32 {
-        match self {
-            Self::Triangle { base, height } => base * height / 2.0,
-            Self::Rectangle { width, height } => width * height,
-            Self::Square { size } => size * size,
-        }
-    }
-}
-
-pub enum BiggerResult {
-    ShapesAreEqual,
-    OneShapeIsBigger(Shape),
-}
-
-pub fn biggest(shape1: Shape, shape2: Shape) -> BiggerResult {
-    let area1 = shape1.area();
-    let area2 = shape2.area();
-
-    if area1 == area2 {
-        BiggerResult::ShapesAreEqual
-    } else if area1 > area2 {
-        BiggerResult::OneShapeIsBigger(shape1)
-    } else {
-        BiggerResult::OneShapeIsBigger(shape2)
-    }
+    let first_half = &s[0..half];
+    let second_half = &s[half..s.len()];
+    println!("{:?} {:?} ", first_half, second_half);
+    first_half == second_half
 }
--- tests/tests.rs
+++ tests/tests.rs
@@ -1,11 +1,12 @@
 use rust_sprint1::*;
 
 #[test]
-fn max_test() {
-    assert_eq!(max(2, 3), 3);
-    assert_eq!(max(5, 4), 5);
-    assert_eq!(max(10, 44), 44);
-    assert_eq!(max(100, 90), 100);
+fn sum_test() {
+    assert_eq!(sum(2, 3), 5);
+    assert_eq!(sum(5, 4), 9);
+    assert_eq!(sum(6, 7), 13);
+    assert_eq!(sum(9, 8), 17);
+    assert_eq!(sum(5, 2), 7);
 }
 
 #[test]
@@ -15,6 +16,14 @@ fn multiply_test() {
     assert_eq!(multiply(6, 7), 42);
     assert_eq!(multiply(9, 8), 72);
     assert_eq!(multiply(5, 2), 10);
+}
+
+#[test]
+fn max_test() {
+    assert_eq!(max(2, 3), 3);
+    assert_eq!(max(5, 4), 5);
+    assert_eq!(max(10, 44), 44);
+    assert_eq!(max(100, 90), 100);
 }
 
 #[test]
@@ -59,30 +68,98 @@ fn nth_prime_test() {
     assert_eq!(nth_prime(23), 83);
 }
 
-/**
- * MOVE SEMANTICS
- * ==============
- */
+#[test]
+fn return_first_member_test() {
+    assert_eq!(return_first_member((4, 5)), 4);
+    assert_eq!(return_first_member((4, 6)), 4);
+    assert_eq!(return_first_member((10, 1)), 10);
+    assert_eq!(return_first_member((10, 4)), 10);
+    assert_eq!(return_first_member((7, 4)), 7);
+}
 
 #[test]
-fn append_char_test() {
+fn sum_members_test() {
+    assert_eq!(sum_members((4, 5, 1)), 10);
+    assert_eq!(sum_members((4, 5, 2)), 11);
+    assert_eq!(sum_members((1, 2, 3)), 6);
+    assert_eq!(sum_members((1, 1, 1)), 3);
+}
+
+#[test]
+fn triplicate_number_test() {
+    assert_eq!(triplicate_number(4), (4, 4, 4));
+    assert_eq!(triplicate_number(410), (410, 410, 410));
+    assert_eq!(triplicate_number(0), (0, 0, 0));
+}
+
+#[test]
+fn triplicate_string_test() {
     assert_eq!(
-        append_char(String::from("hello"), '0'),
-        String::from("hello0")
+        triplicate_string(String::from("hi")),
+        (String::from("hi"), String::from("hi"), String::from("hi"))
     );
     assert_eq!(
-        append_char(String::from("hello"), '1'),
-        String::from("hello1")
+        triplicate_string(String::from("world")),
+        (
+            String::from("world"),
+            String::from("world"),
+            String::from("world")
+        )
     );
 }
 
 #[test]
-fn append_char_mut_test() {
-    let mut s = String::from("hello");
-    append_char_mut(&mut s, '0');
-    assert_eq!(s, String::from("hello0"));
+fn build_array_test() {
+    assert_eq!(build_array(5), [5, 5, 5, 5, 5]);
+    assert_eq!(build_array(1), [1, 1, 1, 1, 1]);
+    assert_eq!(build_array(3), [3, 3, 3, 3, 3]);
+}
 
-    let mut s = String::from("asdf");
-    append_char_mut(&mut s, '1');
-    assert_eq!(s, String::from("asdf1"));
+#[test]
+fn nth_element_test() {
+    assert_eq!(nth_element([1, 2, 3, 4, 5], 0), 1);
+    assert_eq!(nth_element([1, 2, 3, 4, 5], 1), 2);
+    assert_eq!(nth_element([1, 2, 3, 4, 5], 2), 3);
+    assert_eq!(nth_element([1, 2, 3, 4, 5], 3), 4);
+    assert_eq!(nth_element([1, 2, 3, 4, 5], 4), 5);
+}
+
+#[test]
+fn max_in_array_test() {
+    assert_eq!(max_in_array([1, 2, 3, 4, 5]), 5);
+    assert_eq!(max_in_array([10, 10, 3, 4, 5]), 10);
+    assert_eq!(max_in_array([4, 4, 4, 5, 4]), 5);
+    assert_eq!(max_in_array([1, 1, 1, 1, 1]), 1);
+}
+
+#[test]
+fn is_foo_or_bar_test() {
+    assert!(is_foo_or_bar("foo"));
+    assert!(is_foo_or_bar("bar"));
+    assert_eq!(is_foo_or_bar("foor"), false);
+    assert_eq!(is_foo_or_bar("bas"), false);
+    assert_eq!(is_foo_or_bar("f"), false);
+    assert_eq!(is_foo_or_bar("b"), false);
+}
+
+#[test]
+fn contains_char_test() {
+    assert_eq!(contains_char("foo", 'f'), true);
+    assert_eq!(contains_char("foo", 's'), false);
+    assert_eq!(contains_char("bar", 'b'), true);
+    assert_eq!(contains_char("bar", 't'), false);
+}
+
+#[test]
+fn is_first_half_repeated_test() {
+    assert_eq!(is_first_half_repeated("ff"), true);
+    assert_eq!(is_first_half_repeated("fofo"), true);
+    assert_eq!(is_first_half_repeated("fof"), false);
+    assert_eq!(is_first_half_repeated("bar"), false);
+    assert_eq!(is_first_half_repeated("foo"), false);
+    assert_eq!(is_first_half_repeated("asas"), true);
+    assert_eq!(is_first_half_repeated("123451234"), false);
+    assert_eq!(is_first_half_repeated("12341234"), true);
+    assert_eq!(is_first_half_repeated("12241234"), false);
+    assert_eq!(is_first_half_repeated("1234123"), false);
 }
